7602ec5a7a4656bbdcf60e4601d9ad6411e43d5f
尚庆文 <qingwen.shang@tongdun.net>
2022-11-28 18:29:13 +0800

    refactor: 代码模块初始化

---
+++ spring-boot-starter-gargle/src/main/java/com/gargle/starter/SpringBootStarterGargleApplication.java
@@ -0,0 +1,13 @@
+package com.gargle.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterGargleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterGargleApplication.class, args);
+    }
+
+}
---
+++ spring-boot-starter-gargle/src/test/java/com/gargle/starter/SpringBootStarterGargleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gargle.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStarterGargleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
